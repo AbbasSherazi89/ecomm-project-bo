aaad4f8f473375edb325f9ae3de309a4c110a8ea
AbbasSherazi89 <ibne.abbassherazi89@gmail.com>
2025-09-29 10:47:23 +0500

    feat: display product list with api

--- src/app/seller-home/seller-home.ts
+++ src/app/seller-home/seller-home.ts
@@ -1,15 +1,70 @@
 import { Component } from '@angular/core';
-
+import { Product } from '../services/product';
+import { product } from '../seller-type';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-seller-home',
-  imports: [],
-  template: `
-    <p>
-      seller-home works!
-    </p>
+  imports: [CommonModule],
+  template: ` 
+  
+  <div class="product-list">
+    <h1>Product List</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Color</th>
+          <th>Category</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let item of productList">
+          <td><img src="{{item.image}}" alt="Image of the {{item.name}}"></td>
+          <td>{{item.name}}</td>
+          <td>{{item.price}}</td>
+          <td>{{item.color}}</td>
+          <td>{{item.category}}</td>
+          <td>{{item.description}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   `,
-  styles: ``
+  styles: `
+  
+  table{
+    text-align:left;
+    color:blueviolet;
+    border-collapse:collapse;
+
+    img{
+      width:100px;
+      height:100px;
+    }
+    th, td{
+      border:1px solid blueviolet;
+      padding:10px;
+      min-width:160px;
+    }
+  }
+  `,
 })
 export class SellerHome {
+  productList:undefined|product[];
+  constructor(private _product: Product) {}
 
+  ngOnInit() {
+    this._product.productList().subscribe((res) => {
+      console.log(res);
+      if(res){
+        this.productList=res;
+      }
+    });
+  }
+  // getProduct(){
+
+  // }
 }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { product, sellerType } from '../seller-type';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Product {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  addProduct(data:product){
-    console.log("service logged: ", data);
-    
-    return this.http.post(`http://localhost:3000/products`,data);
+  addProduct(data: product) {
+    return this.http.post(`http://localhost:3000/products`, data);
+  }
+  productList(){
+    return this.http.get<product[]>(`http://localhost:3000/products`);
   }
 }
