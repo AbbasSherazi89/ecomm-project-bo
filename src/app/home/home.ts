cfbcb3da51f7aeed72ed97d2276671b87aed96e6
AbbasSherazi89 <ibne.abbassherazi89@gmail.com>
2025-09-30 13:25:22 +0500

    add bootstrap to the project

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,12 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: ``
-})
-export class Home {
+    <div class="mt-4">
+      <button class="btn btn-primary">Primary Button</button>
+      <button class="btn btn-success ms-2">Success Button</button>
+      <button class="btn btn-danger ms-2">Danger Button</button>
 
-}
+      <div class="alert alert-info mt-3">Bootstrap is working!</div>
+
+      <div class="row mt-3">
+        <div class="col-md-6 bg-light p-3">Column 1</div>
+        <div class="col-md-6 bg-secondary text-white p-3">Column 2</div>
+      </div>
+    </div>
+  `,
+  styles: ``,
+})
+export class Home {}
